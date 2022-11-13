8dad3ca1b1910dd192815f7bde6d242d8a823fc3
Wilber Paredes <wilberparedes@gmail.com>
2022-11-13 14:14:43 -0500

    feat: create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Route, Switch } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route path={"/"} exact>
+        <AllMeetupsPage />
+      </Route>
+      <Route path={"/new-meetup"} exact>
+        <NewMeetupPage />
+      </Route>
+      <Route path={"/favorites"} exact>
+        <FavoritesPage />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
