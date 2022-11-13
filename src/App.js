f69360e25ddc4f6a7595d6069a1fd93d15cb8917
Wilber Paredes <wilberparedes@gmail.com>
2022-11-13 14:24:34 -0500

    feat: create link to pages

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import { Route, Switch } from "react-router-dom";
+import MainNavigation from "./components/layout/MainNavigation";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
 
 function App() {
   return (
-    <Switch>
-      <Route path={"/"} exact>
-        <AllMeetupsPage />
-      </Route>
-      <Route path={"/new-meetup"} exact>
-        <NewMeetupPage />
-      </Route>
-      <Route path={"/favorites"} exact>
-        <FavoritesPage />
-      </Route>
-    </Switch>
+    <>
+      <MainNavigation />
+      <Switch>
+        <Route path={"/"} exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path={"/new-meetup"} exact>
+          <NewMeetupPage />
+        </Route>
+        <Route path={"/favorites"} exact>
+          <FavoritesPage />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
